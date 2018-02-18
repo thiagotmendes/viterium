fc3ba0d451f7b7452ab9855fc86aeb3c59b8f155
thiagotmendes <thiagotmendes@outlook.com>
2018-02-18 20:04:28 -0300

    teste de mascara

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   $("#cep").mask("99999-999");
-
+  $('#valor').maskMoney({symbol:"R$",decimal:",",thousands:"."});
   $('.crud-table').DataTable({
     language: {
       url: 'https://cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json'
